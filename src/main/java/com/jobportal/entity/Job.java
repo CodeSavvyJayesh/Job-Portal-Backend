71da807c9bc6b0757466736ffac00709e07b33e6
Jayesh <jayeshdhamal327@gmail.com>
2026-05-21 20:27:50 +0530

    Jobtype Enum and Job Entity created

---
+++ src/main/java/com/jobportal/entity/Job.java
@@ -0,0 +1,44 @@
+package com.jobportal.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "jobs")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false,length = 150)
+    private String title;
+    @Column(nullable = false, length = 120)
+    private String company;
+    @Column(nullable = false, length = 3000)
+    private String description;
+    @Column(nullable = false, length = 100)
+    private String location;
+    private Double salary;
+    @Column(nullable = false)
+    private Integer experienceRequired;
+    @Column(nullable = false, length = 500)
+    private String skills;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private JobType jobType;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    // one recruiter -> many jobs
+    @ManyToOne
+    @JoinColumn(name = "recruiter_id", nullable = false)
+    private User recruiter;
+
+
+}
