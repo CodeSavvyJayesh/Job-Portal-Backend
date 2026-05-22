10d2309139aaa194973da7c585d0ba65359a8ebf
Jayesh <jayeshdhamal327@gmail.com>
2026-05-22 20:17:26 +0530

    Created Job Repository

---
+++ src/main/java/com/jobportal/repository/JobRepository.java
@@ -0,0 +1,16 @@
+package com.jobportal.repository;
+
+import com.jobportal.entity.Job;
+import com.jobportal.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job,Long> {
+    List<Job> findByRecruiter(User recruiter);
+
+    // basically after doing this spring will automatically generates query like:
+    // select * from jobs where recruiter_id i= ?
+}
