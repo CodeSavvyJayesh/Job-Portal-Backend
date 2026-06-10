8ca99a2bca6c239ce9ba049ff364b0c17d39f212
Jayesh <jayeshdhamal327@gmail.com>
2026-06-10 19:58:48 +0530

    cors annotation added

--- src/main/java/com/jobportal/controller/AuthController.java
+++ src/main/java/com/jobportal/controller/AuthController.java
@@ -8,13 +8,11 @@ import com.jobportal.entity.User;
 import com.jobportal.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class AuthController {
     private final UserService userService;
